52cab7536a63d7c566f5d6117f8fc449e22432e5
andrey167 <andrey_167@bk.ru>
2026-09-08 11:14:30 +0400

    aosp16

--- build_script.sh
+++ build_script.sh
@@ -8,8 +8,8 @@ echo "==> Resetting manifests..."
 rm -rf .repo/local_manifests
 
 echo "==> Initializing repo..."
-repo init -u https://github.com/Evolution-X/manifest -b vic --depth=1 --git-lfs
-git clone https://github.com/andrey167/crave_local_manifests -b evox .repo/local_manifests
+repo init -u https://github.com/AndroidOne-Experience/manifest.git -b 16-QPR2 --depth=1 --git-lfs
+git clone https://github.com/andrey167/crave_local_manifests -b aosp16 .repo/local_manifests
 
 ########################################
 # SYNC SOURCE
@@ -25,7 +25,7 @@ echo "==> Syncing source..."
 ########################################
 
 echo "==> Removing old rom zip files..."
-rm -f out/target/product/platina/EvolutionX*.zip
+rm -f out/target/product/platina/AndroidOne*.zip
 rm -f out/target/product/platina/*.zip
 
 echo "==> Preparing environment..."
@@ -43,20 +43,20 @@ grep -n "modemService" packages/services/Telephony/src/com/android/phone/Carrier
 
 git clone https://github.com/andrey167/platinakeys vendor/evolution-priv/keys
 
-grep -q "vendor/evolution-priv/keys/keys.mk" device/xiaomi/platina/BoardConfig.mk || sed -i '$ a -include vendor/evolution-priv/keys/keys.mk' device/xiaomi/platina/lineage_platina.mk
+grep -q "vendor/evolution-priv/keys/keys.mk" device/xiaomi/platina/BoardConfig.mk || sed -i '$ a -include vendor/evolution-priv/keys/keys.mk' device/xiaomi/platina/aosp_platina.mk
 #sed -i 's/PRODUCT_CERTIFICATE_OVERRIDES/PRODUCT_PACKAGE_NAME_OVERRIDES/g' vendor/evolution-priv/keys/keys.mk
-tail -5 device/xiaomi/platina/lineage_platina.mk
+tail -5 device/xiaomi/platina/aosp_platina.mk
 
 echo "==> Lunching target..."
-lunch lineage_platina-bp1a-userdebug
+lunch aosp_platina-bp1a-user
 
 echo "==> Cleaning previous build outputs..."
 m installclean
 
 echo "==> Starting ROM compilation..."
-if ! m evolution; then
+if ! mka bacon; then
     echo "=================================================="
-    echo "==> ERROR: Compilation failed during 'm evolution'!"
+    echo "==> ERROR: Compilation failed during 'mka bacon'!"
     echo "=================================================="
     exit 1
 fi
@@ -65,7 +65,7 @@ fi
 # UPLOAD AND CLEANUP
 ########################################
 
-ZIP_FILE=$(ls -t out/target/product/platina/EvolutionX*.zip 2>/dev/null | head -n 1)
+ZIP_FILE=$(ls -t out/target/product/platina/AndroidOne*.zip 2>/dev/null | head -n 1)
 
 if [ -n "$ZIP_FILE" ] && [ -f "$ZIP_FILE" ]; then
     echo "==> Uploading $ZIP_FILE to GoFile..."
@@ -83,7 +83,7 @@ if [ -n "$ZIP_FILE" ] && [ -f "$ZIP_FILE" ]; then
         if echo "$UPLOAD_RES" | grep -q '"status":"ok"'; then
             DOWNLOAD_PAGE=$(echo "$UPLOAD_RES" | grep -o '"downloadPage":"[^"]*' | cut -d'"' -f4)
             echo "=================================================="
-            echo "GOFILE VANILLA LINK: $DOWNLOAD_PAGE"
+            echo "GOFILE LINK: $DOWNLOAD_PAGE"
             echo "=================================================="
             UPLOAD_SUCCESS=true
         fi
@@ -94,7 +94,7 @@ if [ -n "$ZIP_FILE" ] && [ -f "$ZIP_FILE" ]; then
         PD_RESPONSE=$(curl -s -F "file=@$ZIP_FILE" https://pixeldrain.com/api/file/)
         PD_FILE_ID=$(echo "$PD_RESPONSE" | grep -o '"id":"[^"]*' | cut -d'"' -f4)
         echo "=================================================="
-        echo "PIXELDRAIN VANILLA LINK: https://pixeldrain.com/u/$PD_FILE_ID"
+        echo "PIXELDRAIN LINK: https://pixeldrain.com/u/$PD_FILE_ID"
         echo "=================================================="
     fi
 fi
